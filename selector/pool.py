95a86dc6ed81ef56caf8d6946dbb2d63d1ebfef7
Elias <81258157+EliasSchede@users.noreply.github.com>
2022-02-01 10:48:56 +0100

    Creating scenarios

--- selector/pool.py
+++ selector/pool.py
@@ -6,24 +6,17 @@ from typing import Dict
 @dataclass
 class Configuration:
     id: int
-    conf: list
-    meta_data: dict = field(default_factory=dict)
-    history: dict = field(default_factory=dict)
-    results: dict = field(default_factory=dict)
-    intermidate_results: dict = field(default_factory=dict)
-
-    def add(self, place: str, id, value):
-        assert place not in ["meta_data", "history", "results", "intermidate_results"], "Not a valid method"
-        getattr(self, place)[id] = value
-
+    conf: dict
 
 @dataclass
-class Pool:
-    configurations: Dict[int, Configuration] = field(default_factory=dict)
-    configurations_for_next_run: Dict[int, Configuration] = field(default_factory=dict)
+class Parameter:
+    name: str
+    type: str
+    bound: list
+    default: int
+    condition: list
+    scale: str
 
-    def add_configuration(self, conf):
-        self.configurations[conf.id] = conf
 
-    def get_confguration(self, id):
-        return self.configurations[id]
+def init_pool():
+    pass
---
+++ test/test_scenario.py
@@ -0,0 +1,151 @@
+import unittest
+import numpy as np
+
+from selector.scenario import Scenario, parse_args
+#from selector.scenario import Scenario, parse_args
+#from selector.selector.scenario import Scenario, parse_args
+
+
+
+class Scenario_Test(unittest.TestCase):
+
+    def setUp(self):
+        self.scenario_dict = {'ta_cmd': 'echo test',
+                                   'paramfile': './test_data/test_params.pcs',
+                                   'execdir': '.',
+                                   'deterministic': 0,
+                                   'run_obj': 'runtime',
+                                   'overall_obj': 'mean10',
+                                   'cutoff_time': 300,
+                                   'wallclock_limit': 172800,
+                                   'instance_file':
+                                       './test_data/test_training.txt',
+                                   'test_instance_file':
+                                       './test_data/test_test.txt',
+                                   'feature_file':
+                                       './test_data/test_features.txt',
+                                   'output_dir':
+                                       'placeholder'}
+
+
+    def test_scenario_type(self):
+        with self.assertRaises(TypeError):
+            _ = Scenario(scenario=2)
+
+
+    def test_scenario_from_sting(self):
+        scenario = Scenario('./test_data/test_scenario.txt')
+
+        self.assertEqual(scenario.ta_cmd, 'echo test')
+        self.assertEqual(scenario.execdir, '.')
+        self.assertEqual(scenario.deterministic, "0")
+        self.assertEqual(scenario.run_obj, 'runtime')
+        self.assertEqual(scenario.overall_obj, 'mean10')
+        self.assertEqual(scenario.cutoff_time, '300')
+        self.assertEqual(scenario.wallclock_limit, '172800')
+        self.assertEqual(scenario.instance_file, './test_data/test_training.txt')
+        self.assertEqual(scenario.test_instance_file, './test_data/test_test.txt')
+        self.assertEqual(scenario.feature_file, './test_data/test_features.txt')
+        self.assertEqual(scenario.paramfile,'./test_data/test_params.pcs')
+        self.assertEqual(scenario.instance_set,['./test/test_data/instances/test_instance_1.cnf'])
+        self.assertEqual(scenario.test_instances, ['./test/test_data/instances/test_instance_2.cnf'])
+        self.assertEqual(scenario.feature_names, ['feature_1', ' feature_2'])
+
+        features_1 = scenario.features['./test/test_data/instances/test_instance_1.cnf']
+        self.assertTrue(np.allclose(features_1, [  90., 7650.]))
+
+        features_2 = scenario.features['./test/test_data/instances/test_instance_2.cnf']
+        self.assertTrue(np.allclose(features_2, [  30., 8045.1]))
+
+    def test_scenario_from_dic(self):
+        scenario = Scenario(self.scenario_dict)
+
+        self.assertEqual(scenario.ta_cmd, 'echo test')
+        self.assertEqual(scenario.execdir, '.')
+        self.assertEqual(scenario.deterministic, 0)
+        self.assertEqual(scenario.run_obj, 'runtime')
+        self.assertEqual(scenario.overall_obj, 'mean10')
+        self.assertEqual(scenario.cutoff_time, 300)
+        self.assertEqual(scenario.wallclock_limit, 172800)
+        self.assertEqual(scenario.instance_file,'./test_data/test_training.txt')
+        self.assertEqual(scenario.test_instance_file,'./test_data/test_test.txt')
+        self.assertEqual(scenario.feature_file,'./test_data/test_features.txt')
+        self.assertEqual(scenario.paramfile,'./test_data/test_params.pcs')
+        self.assertEqual(scenario.instance_set,['./test/test_data/instances/test_instance_1.cnf'])
+        self.assertEqual(scenario.test_instances, ['./test/test_data/instances/test_instance_2.cnf'])
+        self.assertEqual(scenario.feature_names, ['feature_1', ' feature_2'])
+
+        features_1 = scenario.features['./test/test_data/instances/test_instance_1.cnf']
+        self.assertTrue(np.allclose(features_1, [  90., 7650.]))
+
+        features_2 = scenario.features['./test/test_data/instances/test_instance_2.cnf']
+        self.assertTrue(np.allclose(features_2, [  30., 8045.1]))
+
+
+    def test_parameter_from_file(self):
+        scenario_file = Scenario('./test_data/test_scenario.txt')
+        scenario_dic = Scenario(self.scenario_dict)
+
+        for scenario in [scenario_file,scenario_dic ]:
+            test_parameter_list = scenario.parameter
+
+            test_no_goods = scenario.no_goods
+
+            # test luby
+            self.assertEqual(test_parameter_list[0].name, 'luby')
+            self.assertEqual(test_parameter_list[0].type, 'boolean')
+            self.assertEqual(test_parameter_list[0].bound, [True, False])
+            self.assertEqual(test_parameter_list[0].default, False)
+            self.assertEqual(test_parameter_list[0].condition, {})
+            self.assertEqual(test_parameter_list[0].scale, '')
+
+            # test rinc
+            self.assertEqual(test_parameter_list[1].name, 'rinc')
+            self.assertEqual(test_parameter_list[1].type, 'cont.')
+            self.assertEqual(test_parameter_list[1].bound, [1.1, 4.0])
+            self.assertEqual(test_parameter_list[1].default, 2)
+            self.assertEqual(test_parameter_list[1].condition, {'luby ': [True, False]})
+            self.assertEqual(test_parameter_list[1].scale, '')
+
+            # test cla-decay
+            self.assertEqual(test_parameter_list[2].name, 'cla-decay')
+            self.assertEqual(test_parameter_list[2].type, 'cont.')
+            self.assertEqual(test_parameter_list[2].bound, [0.9, 0.99999])
+            self.assertEqual(test_parameter_list[2].default, 0.999)
+            self.assertEqual(test_parameter_list[2].condition, {})
+            self.assertEqual(test_parameter_list[2].scale, 'l')
+
+            # test phase-saving
+            self.assertEqual(test_parameter_list[3].name, 'phase-saving')
+            self.assertEqual(test_parameter_list[3].type, 'int')
+            self.assertEqual(test_parameter_list[3].bound, [0, 2])
+            self.assertEqual(test_parameter_list[3].default, 2)
+            self.assertEqual(test_parameter_list[3].condition, {})
+            self.assertEqual(test_parameter_list[3].scale, '')
+
+            # test strSseconds
+            self.assertEqual(test_parameter_list[4].name, 'strSseconds')
+            self.assertEqual(test_parameter_list[4].type, 'cat.')
+            self.assertEqual(test_parameter_list[4].bound, [10.0, 50.0, 100.0, 150.0, 200.0, 250.0, 290.0])
+            self.assertEqual(test_parameter_list[4].default, 150)
+            self.assertEqual(test_parameter_list[4].condition, {'luby ': [True], 'cla-decay ': [0.92, 0.93]})
+            self.assertEqual(test_parameter_list[4].scale, '')
+
+            # test bce-limit
+            self.assertEqual(test_parameter_list[5].name, 'bce-limit')
+            self.assertEqual(test_parameter_list[5].type, 'int')
+            self.assertEqual(test_parameter_list[5].bound,[100000,200000000])
+            self.assertEqual(test_parameter_list[5].default, 100000000)
+            self.assertEqual(test_parameter_list[5].condition, {})
+            self.assertEqual(test_parameter_list[5].scale, 'l')
+
+            # test no goods
+            self.assertEqual(test_no_goods[0], {"luby": True , "rinc": 3})
+
+    def test_instance_file_avail(self):
+        with self.assertRaises(FileExistsError):
+            _ = Scenario(self.scenario_dict, cmd={'check_path': True})
+
+
+
+
