997ffc9550b8a288661b168116c34888890456c0
Elias <81258157+EliasSchede@users.noreply.github.com>
2022-02-01 10:48:56 +0100

    Creating scenarios

---
+++ selector/pool.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, field
+
+from typing import Dict
+
+
+@dataclass
+class Configuration:
+    id: int
+    conf: dict
+
+@dataclass
+class Parameter:
+    name: str
+    type: str
+    bound: list
+    default: int
+    condition: list
+    scale: str
+
+
+def init_pool():
+    pass
