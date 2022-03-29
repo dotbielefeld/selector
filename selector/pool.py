7bcaf43914b9e88dea738bd6cda31026b5438b84
Elias <81258157+EliasSchede@users.noreply.github.com>
2022-03-29 14:09:40 +0200

    Added tournament data class and gender attribute to configuration class

--- selector/pool.py
+++ selector/pool.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-
+import random
 from typing import Dict
 from enum import Enum
 
@@ -8,6 +8,7 @@ from enum import Enum
 class Configuration:
     id: int
     conf: dict
+    gender: str
 
 @dataclass
 class Parameter:
@@ -18,11 +19,27 @@ class Parameter:
     condition: list
     scale: str
 
+@dataclass
+class Tournament:
+    id : int
+    best_finisher: list
+    worst_finisher: list
+    configurations: list
+    configuration_ids: list
+    ray_object_store: dict
+    instance_set: list
+    instance_set_id : int
+
 class ParamType(Enum):
     categorical = 1
     continuous = 2
     integer = 3
 
 
-def init_pool():
-    pass
+class TaskType(Enum):
+    target_algorithm = 1
+    monitor = 2
+
+
+
+
