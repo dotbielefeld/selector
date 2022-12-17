12e8a61a3276bb4c18e64b4cd90c32eef92248a3
Elias <81258157+EliasSchede@users.noreply.github.com>
2022-12-17 09:58:55 +0100

    Get ride of while loop True

--- selector/ta_execution.py
+++ selector/ta_execution.py
@@ -231,8 +231,9 @@ def tae_from_cmd_wrapper_quality(conf, instance_path, cache, ta_command_creator,
         empty_line = False
         memory_p = 0
         cpu_time_p = 0
+        reading = True
 
-        while True:
+        while reading:
             try:
                 line = q.get(timeout=.5)
                 empty_line = False
@@ -253,7 +254,7 @@ def tae_from_cmd_wrapper_quality(conf, instance_path, cache, ta_command_creator,
 
             # Break the while loop when the ta was killed or finished
             if empty_line and p.poll() != None:
-                break
+                reading = False
 
 
         cache.put_result.remote(conf.id, instance_path, float(quality[0]))
